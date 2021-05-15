6079890fa7af53186138879a71cc06c3bd4d75e0
NikkeiR <urnikis.r@gmail.com>
2021-05-15 14:49:28 +0300

    validation

--- src/main/java/VariablesTask.java
+++ src/main/java/VariablesTask.java
@@ -5,13 +5,23 @@ public class VariablesTask {
         triangle(5,6,5);
         triangle(5,5,5);
     }
+
+    static boolean isTriangleValid(int a, int b, int c) {
+        if (a + b > c && a + c > b && b + c > a) {
+            return true;
+        }
+        return false;
+    }
+
     static void triangle(int a, int b, int c) {
-        if (a == b && b == c) {
+        if (isTriangleValid(a,b,c) && a == b && b == c) {
             System.out.println("Lygiakrastis");
-        } else if (a == b || a == c || b == c) {
+        } else if (isTriangleValid(a,b,c) && a == b || a == c || b == c) {
             System.out.println("Lygiasonis");
-        } else {
+        } else if (isTriangleValid(a,b,c) && a != b && b != c) {
             System.out.println("Ivairiakrastis");
+        } else {
+            System.out.println("Netinkamos reiksmes");
         }
     }
 }
