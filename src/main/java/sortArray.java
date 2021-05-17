720b4ee93d85e92b1df7c3bbe10c07ca97a47141
NikkeiR <urnikis.r@gmail.com>
2021-05-17 09:43:02 +0300

    tasks

---
+++ src/main/java/sortArray.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class sortArray {
+    public static void main(String[] args) {
+        int[] array = {-10,0,2,9,-5};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
