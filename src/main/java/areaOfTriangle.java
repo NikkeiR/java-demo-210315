d7166ce2df354ec4a959f333120d6044b6a1fdc2
NikkeiR <urnikis.r@gmail.com>
2021-05-17 11:10:30 +0300

    tasks

--- src/main/java/areaOfTriangle.java
+++ src/main/java/areaOfTriangle.java
@@ -6,15 +6,8 @@ public class areaOfTriangle {
         area(5,5,5);
     }
 
-    static boolean isTriangleValid(int a,int b,int c) {
-        if (a + b > c && a + c > b && b + c > a) {
-            return true;
-        }
-        return false;
-    }
-
     static void area(int a,int b,int c) {
-        if (isTriangleValid(a,b,c)) {
+        if (a + b > c && a + c > b && b + c > a) {
             double p = 0.5 * (a+b+c);
             double triangleArea = Math.sqrt(p * (p - a)*(p - b)*(p - c));
             System.out.println("Plotas = " + triangleArea);
