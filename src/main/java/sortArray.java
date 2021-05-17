a09ecc937cc389c1aebc295df0efc8acfd75ae04
NikkeiR <urnikis.r@gmail.com>
2021-05-17 22:37:18 +0300

    tasks

--- src/main/java/sortArray.java
+++ src/main/java/sortArray.java
@@ -3,7 +3,18 @@ import java.util.Arrays;
 public class sortArray {
     public static void main(String[] args) {
         int[] array = {-10,0,2,9,-5};
-        Arrays.sort(array);
+//        Arrays.sort(array);
+//        System.out.println(Arrays.toString(array));
+        int number;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    number = array[i];
+                    array[i] = array[j];
+                    array[j] = number;
+                }
+            }
+        }
         System.out.println(Arrays.toString(array));
     }
 }
